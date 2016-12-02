35f0a69b9763b45c7fd9e1a7347d8cd7e00a06b7
Erling Tokheim <Erling Tokheim>
2016-12-02 20:59:34 +0100

    PERFORMANCE: loadStoryBatch() into requestAnimationFrame
    requestAnimationFrame:
    	Because it does a visual change to the page.

--- scripts/app.js
+++ scripts/app.js
@@ -314,7 +314,7 @@ APP.Main = (function() {
 		var loadThreshold = (main.scrollHeight - main.offsetHeight -
 				LAZY_LOAD_THRESHOLD);
 		if (main.scrollTop > loadThreshold)
-			loadStoryBatch();
+			requestAnimationFrame(loadStoryBatch);
 	});
 
 	function loadStoryBatch() {
@@ -352,7 +352,7 @@ APP.Main = (function() {
 	// Bootstrap in the stories.
 	APP.Data.getTopStories(function(data) {
 		stories = data;
-		loadStoryBatch();
+		requestAnimationFrame(loadStoryBatch);
 		main.classList.remove('loading');
 	});
 
